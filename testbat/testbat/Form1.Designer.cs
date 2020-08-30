e6ca97c89a792b4af04715d91b01aec4799cb684
wilson840826 <wilson840826@gmail.com>
2020-08-30 15:46:22 +0800

    Add files via upload

---
+++ testbat/testbat/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace testbat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.batchbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(512, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "練習nnimal";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(38, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(333, 232);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(417, 76);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "開啟影像";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.allbutton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(530, 76);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "開啟影像";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // batchbutton
+            // 
+            this.batchbutton.Location = new System.Drawing.Point(417, 142);
+            this.batchbutton.Name = "batchbutton";
+            this.batchbutton.Size = new System.Drawing.Size(75, 23);
+            this.batchbutton.TabIndex = 6;
+            this.batchbutton.Text = "batchbutton";
+            this.batchbutton.UseVisualStyleBackColor = true;
+            this.batchbutton.Click += new System.EventHandler(this.batchbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 400);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.batchbutton);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button batchbutton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ testbat/testbat/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Configuration;
+
+
+namespace testbat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("去執行程式外的檔案\r\n這邊是設定為開啟bat檔");
+            Process pro = new Process();
+            pro.StartInfo.FileName = "C:\\Users\\Public\\Desktop\\Roger9527";
+            pro.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process[] processclose = Process.GetProcessesByName("C:\\Users\\wilso\\Desktop\\test.bat");
+
+        }
+        public void aka()
+        {
+
+        }
+        public void bb()
+        {
+            aka();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("這邊是練習");
+            string word = "我是誰";
+            var ana = new List<animal> //這個list裡面都是animal這個類別的變數
+            {
+                new animal(),new Elephant(),new Wolf()
+            };
+            foreach (var a in ana )
+            {
+                a.speak();
+                MessageBox.Show(a.say + a.dosomething(word));
+            }
+            
+            Wolf bw = new Wolf();
+            MessageBox.Show(Convert.ToString(bw.propoety) + "kg");//這邊直接去get
+            bw.propoety = 50;
+            MessageBox.Show(Convert.ToString ( bw.getweight())+"kg"); //這邊是透過去set 屬性然後用method去呼叫
+            MessageBox.Show(Convert.ToString(bw.propoety)+"kg");//這邊直接去get
+            Elephant ee = new Elephant();
+            ee.speak();
+            ee.call(bw);
+            MessageBox.Show(ee.say);
+            ee.complete(1243);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "C:\\Users\\wilso\\Desktop\\17_turtle.jpg";
+            Bitmap img = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = img;
+        }
+        private void allbutton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "C:\\Users\\wilso\\Desktop\\17_turtle.jpg";
+            Bitmap img = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = img;
+
+            Button btn = (Button)sender;
+            MessageBox.Show(btn.Text);
+
+        }
+
+        private void batchbutton_Click(object sender, EventArgs e)
+        {
+            this.button3.PerformClick();          
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Multiselect = true;
+            op.FileName = "";
+            op.Filter = "txt file (*.*) | *.txt";
+            op.Title = "試試看啦";
+            string firstfile = "";
+            string secondfile = "";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                 firstfile = op.FileNames[0];
+              //   secondfile = op.FileNames[1];
+             //   MessageBox.Show(firstfile + "\r\n" + secondfile);
+            }
+
+
+                 List<string> nl = new List<string>();
+            string[] mm = new string[5];
+            string[] nm = new string[5];
+            for(int a = 0; a<op.FileNames.Length;a++)
+            {
+            nl.Add(op.FileNames[a]);
+            }
+            label1.Text = nl[0]; //nl[0]代表第一個
+            string[] splitstring = nl[0].Split('_');
+            string[] doublestring = splitstring[2].Split('.');
+            string[] doublestring2 = doublestring[0].Split('M');
+            if( doublestring[0] == "M")
+            {
+                nm[0] = nl[0];
+            }
+
+            MessageBox.Show("第一次分割 :" +splitstring[2]);
+            MessageBox.Show("第二次分割 :" + doublestring[0]);
+            MessageBox.Show("第三次分割 :" + doublestring2[0]);
+
+            //理論上可以實踐 依據名稱進行分類
+
+        }
+    }
+}
